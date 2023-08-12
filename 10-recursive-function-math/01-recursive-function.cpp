a71ca599f14aba24a7730818875bf15b0a11253b
Beren <berenickt99@gmail.com>
2023-08-12 12:24:14 +0900

    ADD:10-recursive-function-math

---
+++ 10-recursive-function-math/01-recursive-function.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/** 재귀함수
+ * 재귀함수란 아래의 3가지 특징을 가진 함수를 말합니다.
+ * - 재귀함수(Recursion)는 정의 단계에서 자신을 재참조하는 함수
+ * - 전달되는 상태인 매개변수가 달라질 뿐 똑같은 일을 하는 함수
+ * - 큰 문제를 작은 부분문제로 나눠서 풀 때 사용합니다.
+ *
+ * e.g.) 팩토리얼(factorial)은 1부터 해당 항까지 곱하는 함수입니다.
+ * 이를 재귀함수로 구현하면 다음과 같습니다
+ * n! = n * (n - 1) * (n - 2) * ... * 1
+ */
+int fact_rec(int n) {
+  if (n == 1 || n == 0)
+    return 1;
+  return n * fact_rec(n - 1);
+}
+
+int fact_for(int n) {
+  int ret = 1;
+  for (int i = 1; i <= n; i++) {
+    ret *= i;
+  }
+  return ret;
+}
+
+int n = 5;
+
+int main() {
+  cout << fact_for(n) << '\n';
+  cout << fact_rec(n) << '\n';
+  return 0;
+} // 120 120
---
+++ 10-recursive-function-math/02-recursive-function.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n) {
+  cout << "fibo : " << n << '\n';
+  if (n == 0 || n == 1)
+    return n;
+  return fibo(n - 1) + fibo(n - 2);
+}
+
+int n = 5;
+
+/** 재귀함수를 사용할 때 주의사항
+ * 1. 반드시 기저사례를 써야 한다.
+ * (종료조건) fact를 보면 다음과 같은 기저사례가 있습니다.
+ * if(n == 1 || n == 0) return 1;
+ *
+ * 2. 사이클이 있다면 쓰면 안된다.
+ * e.g.) f(a)가 f(b)를 호출한 뒤 f(b)가 다시 f(a)를 호출하는 것
+ *
+ * 3. 반복문으로 될 거같으면 반복문으로.
+ * (함수호출에 대한 코스트가 듭니다.)
+ */
+int main() {
+  cout << fibo(n) << '\n';
+  return 0;
+}
+/*
+fibo : 5
+fibo : 4
+fibo : 3
+fibo : 2
+fibo : 1
+fibo : 0
+fibo : 1
+fibo : 2
+fibo : 1
+fibo : 0
+fibo : 3
+fibo : 2
+fibo : 1
+fibo : 0
+fibo : 1
+5
+*/
