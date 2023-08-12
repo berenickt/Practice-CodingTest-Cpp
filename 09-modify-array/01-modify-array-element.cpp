8abcae5958200301e9db84888fcc225387339b23
Beren <berenickt99@gmail.com>
2023-08-12 12:17:30 +0900

    ADD:09-modify-array

--- 09-modify-array/01-modify-array-element.cpp
+++ 09-modify-array/01-modify-array-element.cpp
@@ -1,1 +1,23 @@
-// 초기
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[3] = {1, 2, 3};
+void go(int a[]) { a[2] = 100; }
+void go2(int a[3]) { a[2] = 1000; }
+void go3(int *a) { a[2] = 10000; }
+
+/**
+ * Array의 요소를 수정할 때는
+ * 다음처럼 크기를 정하지 않은 int a[], 또는
+ * 배열의 크기인 int a[3], 배열의 포인터인 int * a를 넘겨서 수정할 수 있습니다
+ */
+int main() {
+  go(a);
+  cout << a[2] << '\n'; // 100
+
+  go2(a);
+  cout << a[2] << '\n'; // 1000
+
+  go3(a);
+  cout << a[2] << '\n'; // 10000
+}
--- 09-modify-array/03-modify-two-dimensional-array.cpp
+++ 09-modify-array/03-modify-two-dimensional-array.cpp
@@ -1,1 +1,14 @@
-// 초기
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+void go(int a[][3]) { a[2][2] = 100; }
+void go2(int a[3][3]) { a[2][2] = 1000; }
+
+// 2차원 배열 array로 수정하기
+int main() {
+  go(a);
+  cout << a[2][2] << '\n'; // 100
+  go2(a);
+  cout << a[2][2] << '\n'; // 1000
+}
